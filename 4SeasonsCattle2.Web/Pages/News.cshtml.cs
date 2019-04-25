e9aea82039b37ab42e713413a46fba6cba4262e6
Josh Hill <josh.hill@outlook.es>
2019-04-24 21:05:20 -0400

    Commit.

---
+++ 4SeasonsCattle2.Web/Pages/News.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _4SeasonsCattle.Web.Pages
+{
+    public class NewsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
